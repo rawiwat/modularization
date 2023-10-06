0fbcd765b9a4325187830d9feb38d11256bb64fe
Peter <rawiwatpan@gmail.com>
2023-10-06 09:38:20 +0700

    commit 4

---
+++ ModularizationAssignment/core/common/src/main/java/com/core/common/UiEvent<T>.kt
@@ -0,0 +1,10 @@
+package com.core.common
+
+sealed class UiEvent<T>(
+    val data:T?=null,
+    val message:String? = null
+) {
+    class Loading<T>():UiEvent<T>()
+    class Success<T>(data: T):UiEvent<T>(data = data)
+    class Error<T>(errorMessage:String):UiEvent<T>(message = errorMessage)
+}
--- ModularizationAssignment/feature/movie/domain/src/main/java/com/feature/movie/domain/use_cases/GetYGOListUseCase.kt
+++ ModularizationAssignment/feature/movie/domain/src/main/java/com/feature/movie/domain/use_cases/GetYGOListUseCase.kt
@@ -1,7 +1,13 @@
 package com.feature.movie.domain.use_cases
 
+import com.core.common.UiEvent
+import com.feature.movie.domain.model.YGOCardImage
+import com.feature.movie.domain.repo.YGORepository
 import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class GetYGOListUseCase {
-    //operator fun invoke() = flow<> {}
+class GetYGOListUseCase @Inject constructor(
+    private val ygoRepository: YGORepository
+) {
+    operator fun invoke() = flow<UiEvent<List<YGOCardImage>>> {}
 }
